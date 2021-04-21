76aee35bf098c10bbfdb7e37a45726248aea621c
Bernardo Piuma <bpiuma.uy@gmail.com>
2021-04-21 15:51:31 +0000

    Completo hasta el ejercicio 9

--- .breathecode/resets/05-Create-DOM-Element-First/index.js
+++ .breathecode/resets/05-Create-DOM-Element-First/index.js
@@ -4,8 +4,3 @@ document.querySelector("#myDiv").appendChild(elem);
 
 //Your code here
 // Don'change the code above
-
-let parr = document.createElement("p");
-parr.innerHTML = "Hello World";
-parr.style.background = "yellow";
-document.body.appendChild(parr);
--- exercises/09-Remove-DOM-Element-part-two/index.js
+++ exercises/09-Remove-DOM-Element-part-two/index.js
@@ -1,1 +1,4 @@
-
+let list = document.querySelector("#parentLI");
+let childs = list.childNodes;
+let element = document.querySelector(childs[3]);
+element.parentNode.removeChild(element);
