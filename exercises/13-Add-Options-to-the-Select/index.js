0536ef189e65bc84254157c0ae3a31b6713d8ca3
Bernardo Piuma <bpiuma.uy@gmail.com>
2021-04-22 02:41:52 +0000

    Ultimo ejercicio incompleto

--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -2,5 +2,16 @@ window.onload = function() {
 	console.log("The website was loaded...");
 	let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 
-  // your code here
+	var select = document.querySelector("#mySelect");
+	for (let i = 0; i < countries.length; i++) {
+		let option = document.createElement("option");
+		option.innerHTML = countries[i];
+		select.appendChild(option);
+	}
 };
+
+let change = document.getElementById("mySelect");
+change.addEventListener("change", function() {
+	let opcion = document.querySelector("#mySelect");
+	alert(opcion.value);
+});
--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,12 @@
 // Your code here
+
+let texto = document.getElementById("addToDo");
+texto.addEventListener("change", function() {
+	let valor = texto.value;
+	let tareaNueva = document.createElement("li");
+	tareaNueva.innerHTML = '<span><i class="fa fa-trash"></i></span> ' + valor;
+	let lista = document.getElementsByTagName("ul")[0];
+	lista.appendChild(tareaNueva);
+});
+
+
